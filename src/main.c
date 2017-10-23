d624a697509251e53814d51c6cdc17564ac91cc2
Paul Richard Lettens <plettens@lib-05-l.intra.itcollege.ee>
2017-10-23 14:51:06 +0300

    new  main.c

--- src/main.c
+++ src/main.c
@@ -1,19 +1,35 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define BLINK_DELAY_MS 1000
+#define BLINK_DELAY_MS 500
 
 void main (void)
 {
     /* Set port B pin 7 for output for Arduino Mega yellow LED */
+    DDRA |= _BV(DDA0);
+    DDRA |= _BV(DDA2);
+    DDRA |= _BV(DDA4);
     DDRB |= _BV(DDB7);
+    PORTB &= ~_BV(PORTB7);
 
     while (1) {
-        /* Set port B pin 7 high to turn Arduino Mega yellow LED on */
-        PORTB |= _BV(PORTB7);
+        /* Set port A pin 22 high to turn Arduino Mega red LED on */
+        PORTA |= _BV(PORTA0);
         _delay_ms(BLINK_DELAY_MS);
-        /* Set port B pin 7 high to turn Arduino Mega yellow LED off */
-        PORTB &= ~_BV(PORTB7);
+        /* Set port A pin 22 high to turn Arduino Mega red LED off */
+        PORTA &= ~_BV(PORTA0);
+        _delay_ms(BLINK_DELAY_MS);
+        /* Set port A pin 24 high to turn Arduino Mega green LED on */
+        PORTA |= _BV(PORTA2);
+        _delay_ms(BLINK_DELAY_MS);
+        /* Set port A pin 24 high to turn Arduino Mega green LED off */
+        PORTA &= ~_BV(PORTA2);
+        _delay_ms(BLINK_DELAY_MS);
+        /* Set port A pin 26 high to turn Arduino Mega blue LED on */
+        PORTA |= _BV(PORTA4);
+        _delay_ms(BLINK_DELAY_MS);
+        /* Set port A pin 26 high to turn Arduino Mega blue LED off */
+        PORTA &= ~_BV(PORTA4);
         _delay_ms(BLINK_DELAY_MS);
     }
 }
